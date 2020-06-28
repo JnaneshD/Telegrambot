f06af48a9a30b8b813f6518cc695fd677ca8ede7
jnanesh <jnaneshdana@gmail.com>
2020-06-28 21:59:41 +0530

    not working images commit

--- app.py
+++ app.py
@@ -25,20 +25,20 @@ def respond():
        """
        # send the welcoming message
        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-
+    
 
    else:
-       try:
-           # clear the message we got from any non alphabets
-           text = re.sub(r"\W", "_", text)
-           # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-           # reply with a photo to the name the user sent,
-           # note that you can send photos by url and telegram will fetch it for you
-           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-       except Exception:
+       #try:
+        # clear the message we got from any non alphabets
+        text = re.sub(r"\W", "_", text)
+        # create the api link for the avatar based on http://avatars.adorable.io/
+        url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+        # reply with a photo to the name the user sent,
+        # note that you can send photos by url and telegram will fetch it for you
+        bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+    #except Exception:
            # if things went wrong
-           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+    #      bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
 
    return 'ok'
 
